dd7370e8c2f9348e16d0a91a346507b9da3b3fdb
dohoangtruong <truongdh91@gmail.com>
2020-02-14 17:08:02 +0700

    replace

--- nihonzuki.py
+++ nihonzuki.py
@@ -34,9 +34,9 @@ sheet_headers = {
 	"Accept-Encoding": "gzip, deflate, sdch"
 }
 
-# https://docs.google.com/spreadsheets/d/1hIH7TekqpGIp5QNORti64cZxcobkeacPWHooPB-Q6PE/
+# https://docs.google.com/spreadsheets/d/1H4V5bTRhY9JxgnhGhQ74AnK3Q72N-fWntYmcSQNkkfU/
 def GetSheetIDFromSettings():
-	sid = "1hIH7TekqpGIp5QNORti64cZxcobkeacPWHooPB-Q6PE"
+	sid = "1H4V5bTRhY9JxgnhGhQ74AnK3Q72N-fWntYmcSQNkkfU"
 	resp, content = http.request(get_fshare_setting("GSheetURL"), "HEAD")
 	try:
 		sid = re.compile("/d/(.+?)/").findall(resp["content-location"])[0]
@@ -177,9 +177,9 @@ def getItems(url_path="0", tq="select A,B,C,D,E"):
 		if "plugin://" in item["path"]:
 			if "install-repo" in item["path"]:
 				item["is_playable"] = False
-			elif re.search("plugin.video.tranhuyhoang.playlist/(.+?)/.+?\://", item["path"]):
+			elif re.search("plugin.video.nihonzuki.playlist/(.+?)/.+?\://", item["path"]):
 				match = re.search(
-					"plugin.video.tranhuyhoang.playlist(/.+?/).+?\://", item["path"])
+					"plugin.video.nihonzuki.playlist(/.+?/).+?\://", item["path"])
 				tmp = item["path"].split(match.group(1))
 				tmp[-1] = urllib.quote_plus(tmp[-1])
 				item["path"] = match.group(1).join(tmp)
